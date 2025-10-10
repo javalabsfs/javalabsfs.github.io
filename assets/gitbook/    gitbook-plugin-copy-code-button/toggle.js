a7059ae57381cef6aff1280192f8b1c0663675cd
smhancode <sung-jin-miriam.han@student.uni-tuebingen.de>
2025-10-10 11:10:36 +0200

    d

--- assets/gitbook/    gitbook-plugin-copy-code-button/toggle.js
+++ assets/gitbook/    gitbook-plugin-copy-code-button/toggle.js
@@ -57,6 +57,9 @@ require(["gitbook", "jquery"], function (gitbook, $) {
 
             var $copyCodeButton = $("<button class='copy-code-button'>Copy</button>");
             $copyCodeButton.css({ "position": "absolute", "top": "5px", "right": "5px", "padding": "3px", "background-color": "#ABABAB !important", "color": "white", "border-radius": "5px", "-moz-border-radius": "5px", "-webkit-border-radius": "5px", "border": "2px solid #CCCCCC" });
+            $copyCodeButton[0].style.setProperty("background-color", "#ABABAB", "important");
+            $copyCodeButton[0].style.setProperty("color", "white", "important");
+
             $copyCodeButton.click(function () {
                 var $codeContainer = $(this).siblings("code");
                 if ($codeContainer) {
